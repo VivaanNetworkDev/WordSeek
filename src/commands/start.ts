b176a4cecacce65c5ed6a117b0edc8f3eacb07cf
VivaanNetworkDev <vivaannetwork@gmail.com>
2025-11-04 00:59:17 +0530

    Update start.ts

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -7,7 +7,7 @@ const composer = new Composer();
 
 composer.command("start", async (ctx) => {
   ctx.reply(
-    `<blockquote><strong>WordSeek</strong></blockquote>
+    `<blockquote><strong>𝐖ᴏʀᴅ𝐗ɢᴜᴇ𝐬𝐬ɪɴɢ</strong></blockquote>
 A fun and competitive Wordle-style game that you can play directly on Telegram!
 
 1. Use /new to start a game. Add me to a group with admin permission to play with your friends.

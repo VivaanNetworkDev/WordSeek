936d6415370222f9ebfcafad25aa816c3ec1888d
VivaanNetworkDev <vivaannetwork@gmail.com>
2025-11-03 23:18:15 +0530

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const FOOTER_MESSAGE = `<blockquote>🛠 Developed by Binamra Lamsal\n📢 Official Channel for announcements: @VivaanUpdates\n🗨 Official Group for playing and suggestions: @TheGlobalHub</blockquote>`;
+export const FOOTER_MESSAGE = `<blockquote>🛠 Developed by @Elitesid\n📢 Official Channel for announcements: @VivaanUpdates\n🗨 Official Group for playing and suggestions: @TheGlobalHub</blockquote>`;
 
 export const allowedChatSearchKeys = ["global", "group"] as const;
 export const allowedChatTimeKeys = [
